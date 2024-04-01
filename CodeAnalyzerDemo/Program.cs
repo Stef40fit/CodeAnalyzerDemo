abbc1d6431b0daabb4767fe5ae6a412787c9ccb1
Home <Home@DESKTOP-I95RSCG>
2024-04-01 19:45:37 +0300

    Add project files.

---
+++ CodeAnalyzerDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+/// <summary>
+/// This is main header.
+/// </summary>
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hi!");
+    }
+
+    private string MakeGreeting(string name)
+    {
+        if (name == null)
+        {
+            throw new ArgumentNullException(nameof(name));
+        }
+
+        return "Hello, " + name;
+    }
+}
